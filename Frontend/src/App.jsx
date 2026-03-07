915a2f6cee9758a0b3f960159b3974464d40860c
VeerGupta27 <veer27bc067@satiengg.in>
2026-03-07 17:09:37 +0530

    resolved bug

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,19 +3,31 @@ import { ThemeProvider, useTheme } from './context/ThemeContext';
 
 import { Routes, Route } from "react-router-dom";
 import SignIn from './components/SignIn';
-
+import AppInner from './components/AppInner';
 
 
 export default function App() {
   return (
-    
+   
+    <Routes>
+    <Route
+        path="/signin"
+        element={
+          <>
+            <SignIn />
+          </>
+        }
+      />
 
-     
+      <Route
+        path="/"
+        element={
+      <ThemeProvider>
+      <AppInner />
+     </ThemeProvider>
+    }
+      />
 
       </Routes>
-       </ThemeProvider>
-    </>
-
-
   );
 }
